57fd7590e3d27cc4ed307bc57d417444776ce092
Aaron Meeuwsen <aaronmeeuwsen994@gmail.com>
2025-07-30 14:00:45 -0500

    Create reset.sh

---
+++ reset.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+/usr/local/bin/k3s-killall.sh; /usr/local/bin/k3s-uninstall.sh; rm .kube/config;  rm -rf /etc/rancher/k3s;  rm -rf /var/lib/rancher/k3s
+
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --disable servicelb --token R32pearlz994  --bind-address 192.168.50.200 --disable-cloud-controller --disable traefik
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config; kubectl get pods -A
+
+helm upgrade --install metallb metallb/metallb --create-namespace --namespace metallb-system --wait
+kubectl apply -f https://raw.githubusercontent.com/ameeuwsen/k3s-for-dummies/refs/heads/master/apps/metal.yml
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.18.2/cert-manager.yaml
+kubectl get pods --namespace cert-manager -w
